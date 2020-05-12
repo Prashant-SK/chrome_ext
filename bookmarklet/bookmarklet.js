956d95f732380d1395943d08f1469ade20cb0c53
Prashant-SK <psk.dev.ca@gmail.com>
2020-05-12 16:15:22 -0700

    replace text in p tags

---
+++ bookmarklet/bookmarklet.js
@@ -0,0 +1,10 @@
+
+
+(function(){
+    console.log("bookmarklet starting");
+    let paragraphs = document.getElementsByTagName('p');
+    for (let i = 0; i < paragraphs.length; i++) {
+        paragraphs[i].innerHTML = 'New Text';
+    }
+})();
+
