885f954a539cce5e62ff2d180426b4f7e890ab68
Prashant-SK <psk.dev.ca@gmail.com>
2020-05-19 14:19:17 -0700

    Relay data between content and background scripts

--- basic/content.js
+++ basic/content.js
@@ -4,3 +4,9 @@ let paragraphs = document.getElementsByTagName('p');
 for (elt of paragraphs) {
     elt.style['background-color'] = '#FF00FF';
 }
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, response) {
+    console.log(message.msg);
+}
