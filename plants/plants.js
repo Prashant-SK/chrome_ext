d8ab1a926617dbe0f739c1740f546922185e6641
Prashant-SK <psk.dev.ca@gmail.com>
2020-05-20 20:37:10 -0700

    completed plant extension

--- plants/plants.js
+++ plants/plants.js
@@ -1,1 +1,20 @@
 console.log("Plants!");
+
+let fileNames = [
+    "plant1.png",
+    "plant2.png",
+    "plant3.png",
+    "plant4.png",
+    "plant5.png"
+];
+
+let images = document.getElementsByTagName('img');
+
+for (elem of images) {
+    let i = Math.floor(Math.random() * fileNames.length);
+    let index = 'plantImages/' + fileNames[i];
+    let url = chrome.extension.getURL(index);
+        elem.src = url;
+    console.log(url);
+} 
+
