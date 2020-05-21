d80cdf613c37f76ed0da3d897f19487e5115efb1
Prashant-SK <psk.dev.ca@gmail.com>
2020-05-20 19:28:01 -0700

    added image assets for plants extension

--- basic/content.js
+++ basic/content.js
@@ -1,12 +1,14 @@
 console.log("Chrome ext go?");
 
-let paragraphs = document.getElementsByTagName('p');
-for (elt of paragraphs) {
-    elt.style['background-color'] = '#FF00FF';
-}
-
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, response) {
     console.log(message.msg);
+
+    if (message.msg = "hello"){
+        let paragraphs = document.getElementsByTagName('p');
+        for (elt of paragraphs) {
+            elt.style['background-color'] = '#FF00FF';
+        }
+    }
 }
