2beb5f1137024bca4804dd298855a528d8b7fbea
Prashant-SK <psk.dev.ca@gmail.com>
2020-06-12 18:31:46 -0700

    fixed bug in content script

--- wordDef/background.js
+++ wordDef/background.js
@@ -1,3 +1,7 @@
 console.log("background script active");
 
 chrome.runtime.onMessage.addListener(receiver);
+
+function receiver(request, sender, sendResponse) {
+    console.log(request);
+}
--- wordDef/content.js
+++ wordDef/content.js
@@ -4,11 +4,11 @@ window.addEventListener('mouseup', wordSelected);
 
 function wordSelected() {
     let selectedText = window.getSelection().toString();
-    console.log(selected);
+    console.log(selectedText);
     if(selectedText.length > 0) {
         let message = {
             text: selectedText
-        }
+        };
         chrome.runtime.sendMessage();
     }
 }
